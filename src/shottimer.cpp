1a01c45095f5d23642647ec8cc8121a6a0137347
Jason Sipula <alupis1@gmail.com>
2018-07-31 23:25:21 -0700

    Will have the main.cpp hook into the program runtime, launching the main app

---
+++ src/shottimer.cpp
@@ -0,0 +1,18 @@
+/**
+ * Shot Timer
+ *
+ * Copyright (C) 2018 Jason Sipula <alupis1@gmail.com>
+ *
+ */
+
+#include "shottimer.h"
+
+void initialize()
+{
+
+}
+
+void control_loop()
+{
+    
+}
---
+++ src/shottimer.h
@@ -0,0 +1,21 @@
+/**
+ * Shot Timer
+ * 
+ * Shot Timer Header
+ *
+ * Copyright (C) 2018 Jason Sipula <alupis1@gmail.com>
+ *
+ */
+
+#ifndef __SHOT_TIMER_H__
+#define __SHOT_TIMER_H__
+
+/**
+ * To Enable Debug Logging, uncomment this
+ */
+#define __DEBUG_LOGGING_ENABLED__
+
+void initialize(void);
+void control_loop(void);
+
+#endif
